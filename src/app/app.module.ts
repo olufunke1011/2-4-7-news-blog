d0685b05c33278ca11e9293039b1de1d539e1f89
olufunke1011 <owoseniolufunke1011@gmail.com>
2020-12-08 12:45:20 +0100

    craeting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { PoliticsComponent } from './politics/politics.component';
+import { BusinessComponent } from './business/business.component';
+import { SportsComponent } from './sports/sports.component';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
+import { HealthComponent } from './health/health.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NewsComponent,
+    PoliticsComponent,
+    BusinessComponent,
+    SportsComponent,
+    EntertainmentComponent,
+    HealthComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
