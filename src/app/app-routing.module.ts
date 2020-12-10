2d7510ade1d1564f6081c74428861dadd52313dd
elotachukwu <elotachukwu@gmail.com>
2020-12-10 17:20:56 +0100

    Routing entertainment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BusinessComponent } from './business/business.component';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
 
 const routes: Routes = [
   {
-    path: 'business',
+    path: "business",
     component: BusinessComponent
   },
+
+  {
+    path: "entertainment",
+    component: EntertainmentComponent
+  }
 ];
 
 @NgModule({
